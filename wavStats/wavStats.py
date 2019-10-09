7ada5415991f04645a0f3fc6434c78a42796bdad
David Mann <dmann@loggerhead.com>
2019-10-09 15:27:15 -0400

    Python convert to Pascal example

---
+++ wavStats/wavStats.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  9 15:10:07 2019
+
+@author: dmann
+"""
+
+import matplotlib.pyplot as plt
+import scipy.io.wavfile as wav
+import numpy as np
+
+# hydrophone calibration dBV re 1uPascal
+hydroCal = -180
+
+# board gain dB full-scale referenced to 1.0 V
+boardGain = 2.0
+
+sensitivityDb = hydroCal + boardGain
+
+# calculate V / uPa
+sensitivityLinear = np.power(10, sensitivityDb/20.0)
+
+path = '/Users/dmann/w/learn/wavStats/'
+
+fileName = 'FMsweep.wav'
+s, y = wav.read(path + fileName)
+
+# scale to +/- 1.0
+y = y / 32768.0
+
+# remove DC offset (optional)
+y = y - np.mean(y)
+
+# scale to Pascals
+yMicroPascals = y / sensitivityLinear
+yPascals = yMicroPascals / 1000000
+
+peakPascals = max(yPascals)
+peakDb = 20 * np.log10(peakPascals * 1000000)
+
+print('Peak Pascals')
+print(peakPascals)
+print()
+print('Peak dB re 1uPa')
+print(peakDb)
+
+plt.plot(yPascals)
+plt.ylabel('Pa')
+plt.show()
